137e93869b5d7593b133319dbcf39ab710aec52d
336-Daniel <e0850405309@uisrael.edu.ec>
2026-07-17 21:54:05 -0500

    Parte A — Configuración de Keycloak COMPLETADA

---
+++ ClassBooking-service/src/test/java/com/uti/classbookingservice/ClassBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uti.classbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
